4bf0cd0a31f835a8f674a17ece5e058f320651e4
Nikolay Stoitsov <nikolay.stoitsov@haw-hamburg.de>
2016-10-10 14:50:10 +0200

    Updated: shifting added(right, down and both); FFT is displayed

--- imageOperations.m
+++ imageOperations.m
@@ -18,53 +18,82 @@ classdef imageOperations
         function r = rotate90cw(obj)
             for i = 1:obj.width
                 for j = 1:obj.height
-                    image1(i,j) = obj.image(obj.height-j+1,i);
+                    outputImage(i,j) = obj.image(obj.height-j+1,i);
                 end
             end 
-            r = image1;
+            r = outputImage;
         end
 % rotate 90 degrees counter clockwise        
         function r = rotate90ccw(obj)
             for i = 1:obj.width
                 for j = 1:obj.height
-                    image1(i,j) = obj.image(j, obj.width-i+1);
+                    outputImage(i,j) = obj.image(j, obj.width-i+1);
                 end
             end 
-            r = image1;
+            r = outputImage;
         end
 % rotate 180 degrees      
         function r = rotate180(obj)
-            image1 = obj.image(obj.height:-1:1,obj.width:-1:1);
-            r = image1;
+            outputImage = obj.image(obj.height:-1:1,obj.width:-1:1);
+            r = outputImage;
         end
 % mirror left-right       
         function r = mirrorlr(obj)
-            image1 = obj.image(:,obj.width:-1:1);
-            r = image1;
+            outputImage = obj.image(:,obj.width:-1:1);
+            r = outputImage;
         end
 % mirror up-down
         function r = mirrorud(obj)
-            image1 = obj.image(obj.height:-1:1,:);
-            r = image1;
+            outputImage = obj.image(obj.height:-1:1,:);
+            r = outputImage;
         end
 % cropping 
-        function r = crop(obj, newHeight, newWidth)     % newHeight and newWidth are vectors
-            image1 = obj.image(newHeight, newWidth);    %(1:100, 300:512 );    %rows 1:100, cols 300:512
-            r = image1;
-        end
-% mask out selected area of the image
-        function r = maskout(obj, height, width)
-            image1 = ones(obj.height,obj.width);        %white image that will not affect the original
-            image1(height,width) = 0;                   %area to be masked out
-            image1 = image1.*im2double(obj.image);      %multiplying the original with the mask image
-            r = image1;
+        function r = crop(obj, height, width)
+            outputImage = ones(obj.height,obj.width);        %white image that will not affect the original
+            outputImage(height,width) = 0;                   %area to be masked out
+            outputImage = outputImage.*im2double(obj.image); %multiplying the original with the mask image
+            r = outputImage;
         end
 % keep the selected area and mask out the rest
-        function r = maskoutside(obj, height, width)
-            image1 = zeros(obj.height,obj.width);
-            image1(height,width) = 1;
-            image1 = image1.*im2double(obj.image);
-            r = image1;
+        function r = maskout(obj, height, width)
+            outputImage = zeros(obj.height,obj.width);
+            outputImage(height,width) = 1;
+            outputImage = outputImage.*im2double(obj.image);
+            r = outputImage;
+        end
+% shift right
+        function r = shiftRight(obj, width)
+            if width > obj.width
+                warning('Width of shift exceeds image width');
+            end
+            shifted = obj.image(:, 1 : obj.width - width);
+            outputImage = zeros(obj.height, obj.width);
+            outputImage(:, width+1:obj.width) = im2double(shifted);
+            r = outputImage;
+        end
+% shift down        
+        function r = shiftDown(obj, height)
+            if height > obj.height
+                warning('Height of shift exceeds image height');
+            end
+            shifted = obj.image(1 : obj.height-height, :);
+            outputImage = zeros(obj.height, obj.width);
+            outputImage(height+1:obj.height, :) = im2double(shifted);
+            r = outputImage;
+        end
+% shift right and down       
+        function r = shiftRightDown(obj, height, width)
+            if height > obj.height
+                warning('Height of shift exceeds image height');
+            end
+            if width > obj.width
+                warning('Width of shift exceeds image width');
+            end
+            shifted_w = obj.image(:, 1 : obj.width - width);
+            shifted = shifted_w(1 : obj.height-height, :);
+            outputImage = zeros(obj.height, obj.width);
+            outputImage(height+1:obj.height, width+1:obj.width) = im2double(shifted);
+            r = outputImage;
         end
     end
 end
--- testImageOperations.m
+++ testImageOperations.m
@@ -1,6 +1,7 @@
 clear
 clc
-image = imread('http://www.doc.gold.ac.uk/~mas02fl/MSC101/ImageProcess/defect03_files/fig_2_3_14.jpg');
+% image = imread('http://www.doc.gold.ac.uk/~mas02fl/MSC101/ImageProcess/defect03_files/fig_2_3_14.jpg');
+image = imread('.\OutputImages\diagonal_stripes.jpg');
 class = @imageOperations;
 A = class(image);
 
@@ -8,7 +9,8 @@ A = class(image);
 op = input(['Select an operation to be performed\n 1 - rotate 90 deg clockwise\n'...
             ' 2 - rotate 90 deg counter clockwise\n 3 - rotate 180 deg\n'...
             ' 4 - mirror left-right\n 5 - mirror up-down\n 6 - crop,\n'...
-            ' 7 - mask out selected area\n 8 - mask outside selected area\n']);
+            ' 7 - mask out selected area\n 8 - right shift\n 9 - down shift\n'...
+            ' 10 - right and down shift\n']);
         
 switch op
     case 1
@@ -30,18 +32,31 @@ switch op
         w = input('Width (e.g. 100:300): ');
         img = A.maskout(h, w);
     case 8
-        h = input('Height (e.g. 100:300): ');
-        w = input('Width (e.g. 100:300): ');
-        img = A.maskoutside(h, w);
+        w = input('Size of right shift (e.g. 100): ');
+        img = A.shiftRight(w);
+    case 9
+        h = input('Size of down shift (e.g. 100): ');
+        img = A.shiftDown(h);
+    case 10
+        h = input('Size of down shift (e.g. 100): ');
+        w = input('Size of right shift (e.g. 100): ');
+        img = A.shiftRightDown(h,w);
     otherwise
-        h = msgbox('Invalid Operation', 'Error','error');
+        msgbox('Invalid Operation', 'Error','error');
         return;
 end
+f_original = mat2gray(abs(fftshift(fft2(image))));
+f_modified = mat2gray(abs(fftshift(fft2(img))));
 figure
-subplot(1,2,1)
+subplot(2,2,1)
 imshow(image)
 title('Original image')
-subplot(1,2,2)
+subplot(2,2,3)
+imshow(f_original)
+title('FFT of original');
+subplot(2,2,2)
 imshow(img)
 title('Modified image')
-            
+subplot(2,2,4)
+imshow(f_modified)
+title('FFT of modified')
