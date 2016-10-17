73323d2aecb9198bc22415aa29ff4eebdebec4b5
Nikolay Stoitsov <nikolay.stoitsov@haw-hamburg.de>
2016-10-17 22:00:26 +0200

    Resampling added in test file. In imageOperations.m no code changed, just some formatting issues

--- imageOperations.m
+++ imageOperations.m
@@ -7,75 +7,75 @@ classdef imageOperations
     methods (Access=private) % methods only used by other methods inside
 
         %re-using existing Low-pass filter functionality
-        function r=applyLowPass(obj, frequency)
-            filterInstance=cj2Filter(obj.height,obj.width);
+        function r = applyLowPass(obj, frequency)
+            filterInstance = cj2Filter(obj.height, obj.width);
             filterInstance = filterInstance.lowPass(frequency); %instantiation
             %application of a filter
-            filteredImage=cj2Transformation.filter(filterInstance,obj.image);
+            filteredImage = cj2Transformation.filter(filterInstance, obj.image);
             % output of filter is complex, so we transform it to uint8 format
-            r= uint8(abs(filteredImage));
+            r = uint8(abs(filteredImage));
         end
      
-        function r=sampleDown(obj, ratio, antialiasing)
-            
+        function r = sampleDown(obj, ratio, antialiasing)
             % applying antialiasing (low-pass) filter if user said so
             if (antialiasing)
                 % picking smaller dimesnsion of an image to use it as a
                 % reference value to our filter dimensioning
-                smallerDimension=min(obj.width, obj.height);
+                smallerDimension = min(obj.width, obj.height);
                 % to avoid aliasing, maximum frequency in the resulting
                 % image should be at least two times smaller than amount of
                 % samples (smallerDimension*ratio)
-                intermediateImage=applyLowPass(obj, floor(smallerDimension*ratio/2));
+                intermediateImage = applyLowPass(obj, floor(smallerDimension*ratio/2));
             else
-                intermediateImage=obj.image;
+                intermediateImage = obj.image;
             end
-           
+            
             % creating an array of indexes of rows/coloumns that should be left
             % in place, compressing original dimensions to smaller ones
             step = 1/ratio;
-            X=ceil([1:step:obj.height]);
-            Y=ceil([1:step:obj.width]);
+            X = ceil(1:step:obj.height);
+            Y = ceil(1:step:obj.width);
             
             % creating new matrix using only rows/coloumns that we chose
             r = intermediateImage(X,Y);
         end
         
-        function r=sampleUp(obj, ratio)
-            
+        function r = sampleUp(obj, ratio)
             %defining a size of a new image
-            newHeight=ceil(obj.height*ratio);
-            newWidth=ceil(obj.width*ratio);
+            newHeight = ceil(obj.height*ratio);
+            newWidth = ceil(obj.width*ratio);
             
             %initializing new image to avoid memory reallocation
             %formatting it to uint8 as it is double by default
-            r =uint8(zeros(newHeight, newWidth));
+            r = uint8(zeros(newHeight, newWidth));
             
             %pad original image with zeroes to avoid missing the bounds of
             %an array while apllying bipolar interpolations
-            pImg=zeros(obj.height+2, obj.width+2);
-            pImg(2:end-1,2:end-1)=obj.image;
+            pImg = zeros(obj.height+2, obj.width+2);
+            pImg(2:end-1, 2:end-1) = obj.image;
             
             step = 1/ratio;
             function r = interpolatePoint(i,j)
                 % defining indexes of initial matrix values, that are
                 % "surrounding" our new point
                 % shifting by 1 as it is padded
-                initX=floor(i*step)+1; initY=floor(j*step)+1;
+                initX = floor(i*step) + 1; 
+                initY = floor(j*step) + 1;
                 % defining the distance between our new point and initial
                 % points of the matrix
-                diffX=initX-i*step; diffY=initY-j*step;
+                diffX = initX - i*step; 
+                diffY = initY - j*step;
                 
                 %interpolating horizontally, note that distance between
                 %points in original matrix is 1
-                y0= pImg(initX,initY)*diffX + pImg(initX+1,initY)*(1-diffX);
-                y1= pImg(initX,initY+1)*diffX + pImg(initX+1,initY+1)*(1-diffX);
+                y0 = pImg(initX, initY)*diffX + pImg(initX+1, initY)*(1-diffX);
+                y1 = pImg(initX, initY+1)*diffX + pImg(initX+1, initY+1)*(1-diffX);
                 %interpolating vertically
-                r= uint8(y0*diffY + y1*(1-diffY));
+                r = uint8(y0*diffY + y1*(1-diffY));
             end
             
-            for i = 1: newHeight
-                for j=1:newWidth
+            for i = 1:newHeight
+                for j = 1:newWidth
                     r(i,j)=interpolatePoint(i,j);
                 end
             end
@@ -83,7 +83,7 @@ classdef imageOperations
     end
     methods
         %Constructor
-        function obj=imageOperations(img)
+        function obj = imageOperations(img)
             [obj.height, obj.width, d] = size(img);
             if d == 3
                 obj.image = rgb2gray(img);
@@ -92,7 +92,7 @@ classdef imageOperations
             end
         end
         % resample without anti-aliasing filter
-        function r = resample(obj, ratio,antialiasing)
+        function r = resample(obj, ratio, antialiasing)
             % making antialiasing an optional parameter, initializing it as
             % false if it doesn't exist
             if ~exist('antialiasing','var')
@@ -100,53 +100,59 @@ classdef imageOperations
             end
             % applying upsampling or downsampling depending on the ratio
             % different algorithms are used
-            if(ratio<=1) r =sampleDown(obj,ratio,antialiasing); end
-            if (ratio>1) r = sampleUp(obj,ratio); end
+            if ratio <= 1
+                r = sampleDown(obj, ratio, antialiasing); 
+            end
+            if ratio > 1 
+                r = sampleUp(obj, ratio); 
+            end
         end
         % rotate 90 degrees clockwise
         function r = rotate90cw(obj)
+            outputImage = zeros(obj.height, obj.width);
             for i = 1:obj.width
                 for j = 1:obj.height
-                    outputImage(i,j) = obj.image(obj.height-j+1,i);
+                    outputImage(i, j) = obj.image(obj.height-j+1, i);
                 end
             end
-            r = outputImage;
+            r = uint8(outputImage);
         end
         % rotate 90 degrees counter clockwise
         function r = rotate90ccw(obj)
+            outputImage = zeros(obj.height, obj.width);
             for i = 1:obj.width
                 for j = 1:obj.height
-                    outputImage(i,j) = obj.image(j, obj.width-i+1);
+                    outputImage(i, j) = obj.image(j, obj.width-i+1);
                 end
             end
-            r = outputImage;
+            r = uint8(outputImage);
         end
         % rotate 180 degrees
         function r = rotate180(obj)
-            outputImage = obj.image(obj.height:-1:1,obj.width:-1:1);
+            outputImage = obj.image(obj.height:-1:1, obj.width:-1:1);
             r = outputImage;
         end
         % mirror left-right
         function r = mirrorlr(obj)
-            outputImage = obj.image(:,obj.width:-1:1);
+            outputImage = obj.image(:, obj.width:-1:1);
             r = outputImage;
         end
         % mirror up-down
         function r = mirrorud(obj)
-            outputImage = obj.image(obj.height:-1:1,:);
-            r = outputImage;
-        end
-        % cropping
-        function r = crop(obj, height, width)
-            outputImage = ones(obj.height,obj.width);        %white image that will not affect the original
-            outputImage(height,width) = 0;                   %area to be masked out
-            outputImage = outputImage.*im2double(obj.image); %multiplying the original with the mask image
+            outputImage = obj.image(obj.height:-1:1, :);
             r = outputImage;
         end
         % keep the selected area and mask out the rest
         function r = maskout(obj, height, width)
-            outputImage = zeros(obj.height,obj.width);
-            outputImage(height,width) = 1;
+            outputImage = ones(obj.height, obj.width);        %white image that will not affect the original
+            outputImage(height, width) = 0;                   %area to be masked out
+            outputImage = outputImage.*im2double(obj.image);  %multiplying the original with the mask image
+            r = outputImage;
+        end
+        % cropping
+        function r = crop(obj, height, width)
+            outputImage = zeros(obj.height, obj.width);
+            outputImage(height, width) = 1;
             outputImage = outputImage.*im2double(obj.image);
             r = outputImage;
         end
--- testImageOperations.m
+++ testImageOperations.m
@@ -1,7 +1,7 @@
 clear
 clc
 % image = imread('http://www.doc.gold.ac.uk/~mas02fl/MSC101/ImageProcess/defect03_files/fig_2_3_14.jpg');
-image = imread('.\OutputImages\diagonal_stripes.jpg');
+image = imread('.\OutputImages\horizontal_stripes.jpg');
 class = @imageOperations;
 A = class(image);
 
@@ -10,7 +10,7 @@ op = input(['Select an operation to be performed\n 1 - rotate 90 deg clockwise\n
             ' 2 - rotate 90 deg counter clockwise\n 3 - rotate 180 deg\n'...
             ' 4 - mirror left-right\n 5 - mirror up-down\n 6 - crop,\n'...
             ' 7 - mask out selected area\n 8 - right shift\n 9 - down shift\n'...
-            ' 10 - right and down shift\n']);
+            ' 10 - right and down shift\n 11 - resample\n']);
         
 switch op
     case 1
@@ -41,6 +41,17 @@ switch op
         h = input('Size of down shift (e.g. 100): ');
         w = input('Size of right shift (e.g. 100): ');
         img = A.shiftRightDown(h,w);
+    case 11
+        ratio = input('Resize ratio: ');
+        aa = input('Use anti-aliasing filter (y/n): ', 's');
+        if aa == 'y'
+           img = A.resample(ratio, true);
+        elseif aa == 'n'
+           img = A.resample(ratio);         % 2nd param is optional, false by default
+        else
+           warning('Invalid input! Anti-aliasing filter will NOT be used.');
+           img = A.resample(ratio); 
+        end
     otherwise
         msgbox('Invalid Operation', 'Error','error');
         return;
