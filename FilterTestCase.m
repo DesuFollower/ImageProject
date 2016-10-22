72b6c04e8bbbee460975d685d844a22fc0029a09
Nikolay Stoitsov <nikolay.stoitsov@haw-hamburg.de>
2016-10-22 16:03:26 +0200

    Trunk update

--- FilterTestCase.m
+++ FilterTestCase.m
@@ -1,50 +1,40 @@
 clear;
 clc;
-height=200;
-width=200;
-signalF=10;
+height=100;
+width=100;
+signalF=5;
 filterCutoff=8;
-filterCutoffTwo=30;
+filterCutoffTwo=15;
 image=sampleImage(height,width);
-image=image.horizontalStripes(signalF);
+image=1/2.*(image.horizontalStripes(signalF)+image.horizontalStripes(4+signalF).*1);
+
 figure(1)
 subplot(2,3,1)
 imshow(image);
-title('Original Image');
-
-subplot(2,3,4)
-image_fft=fftshift(fft2(image));
-imshow(mat2gray(abs(image_fft)));
 title('Magnitude of FFT Original');
-
+  
 subplot(2,3,2)
-<<<<<<< HEAD
 filterInstance=cj2Filter(height,width);
-filterInstance = filterInstance.bandStop(filterCutoff,filterCutoffTwo);
-% filterInstance = filterInstance.highPass(filterCutoff);
+%filterInstance = filterInstance.bandStop(filterCutoff,filterCutoffTwo);
+filterInstance = filterInstance.lowPass(filterCutoff);
 filterTimedomain=fftshift(ifft2(filterInstance));
-=======
-lowPassFilter=cj2Filter(height,width);
-lowPassFilter = lowPassFilter.bandStop(filterCutoff,filterCutoffTwo);
-filterTimedomain=fftshift(ifft2(lowPassFilter));
->>>>>>> 28691185e8dd194222c7248be7d46839e3d47fbb
 %Fitting the spectrum in 0...255
 scalingFactor=floor(255./max(max(abs(filterTimedomain))));
 imshow(uint8(scalingFactor.*abs(filterTimedomain)));
+%imshow(uint8(scalingFactor.*filterTimedomain));
 title('Time Domain of FFT filter');
-
-
+  
+  
 subplot(2,3,5)
-imshow(uint8(255*lowPassFilter));
+imshow(uint8(255*filterInstance));
 title('Magnitude of FFT filter');
-
-
+  
+  
 subplot(2,3,3)
-filteredImage=cj2Transformation.filter(lowPassFilter,image);
-imshow(uint8(filteredImage));
+filteredImage=cj2Transformation.filter(filterInstance,image);
+%imshow(uint8(abs(filteredImage)));
+scalingFactor=floor(255./max(max(abs(filteredImage))));
+imshow(uint8(scalingFactor.*abs(filteredImage)));
 title('Filtered Image');
-
+  
 subplot(2,3,6)
-filteredImage_fft=fft2(filteredImage);
-imshow(mat2gray(255*abs(filteredImage_fft)));
-title('Magnitude of Transformed FFT');
