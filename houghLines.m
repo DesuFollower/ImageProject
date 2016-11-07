41f1bd4908434ee18f5269f9ab8e29d7bd0fac82
Ervin Madaha <vicervin95@gmail.com>
2016-11-07 22:00:37 +0100

    houghLines.m -Edited filters:: now using gaussian filter and local maxima function

--- houghLines.m
+++ houghLines.m
@@ -1,11 +1,8 @@
+close all;
 clear;
 clc;
 
 image=rgb2gray(imread('lines.jpg'));
- if(abs(R15-(xi*cos(Fi(radiFy(2,i)))+yi*sin(Fi(radiFy(2,i)))))<threshold)
-                    lines(xi,yi)=255;
-               
-
 canniedImage=edge(image,'canny');
 
 figure(1)
@@ -15,7 +12,7 @@ subplot(2,1,2);
 %figure(2)
 imshow(canniedImage);
 
-steps=50;
+steps=200;
 dFi=2*pi/steps;
 Fi=zeros(steps);
 Fi=0:dFi:2*pi-dFi;
@@ -25,7 +22,7 @@ maxR=sqrt(width*width+height*height);
 dr=maxR/steps;
 R=0:dr:maxR-dr;
 
-threshold=3;
+threshold=1;
 votingSpace=zeros(steps,steps);
 
 for xi=1:height
@@ -33,7 +30,7 @@ for xi=1:height
         if(canniedImage(xi,yi))
             for iR=1:steps
                 for iFi=1:steps
-                    if(abs(R(iR)-(xi*cos(Fi(iFi))+yi*sin(Fi(iFi))))<threshold)
+                    if(abs(R(iR)-(xi*cos(Fi(iFi))+yi*sin(Fi(iFi))))<=threshold)
                         votingSpace(iR,iFi)=votingSpace(iR,iFi)+1;
                     end
                 end
@@ -42,42 +39,40 @@ for xi=1:height
    end
 end
 
-spectre = votingSpace;
-ultima=max(max(spectre));
-radiFy=zeros(2,steps);
+preMaximus =imgaussfilt(votingSpace,0.01);
+Maximus=imregionalmax(preMaximus);
+numberOfLines =sum(sum(Maximus));
+radiFy=zeros(numberOfLines,2);
 index=1;
-while ((ultima/4)<(max(max(spectre))))
-    
-    [val,y]=max(spectre);
-    [trueMax,x]=max(val);
-    thresh=3;
-    for ty=-thresh:thresh
-        for tx=-thresh:thresh 
-            spectre(mod((y(x)+ty-1),steps)+1,mod((x+tx-1),steps)+1)= 0;
+for iR=1:steps
+    for iFi=1:steps
+        if (Maximus(iR,iFi))
+           radiFy(index,:)=[iR iFi];
+           index=index+1;
         end
-    end    
-    radiFy(1,index)=y(x);
-    radiFy(2,index)=x;
-    index=index+1;
-end    
+    end
+end   
 
 lines=zeros(height,width);
 for xi=1:height
    for yi=1:width
-        for i = 1:index-1
-            if(canniedImage(xi,yi))
-
-                if(abs(R(radiFy(1,i))-(xi*cos(Fi(radiFy(2,i)))+yi*sin(Fi(radiFy(2,i)))))<threshold)
-                    lines(xi,yi)=255;
-                end
-            end
-        end
+            %if(canniedImage(xi,yi))
+            for iPoint=1:numberOfLines
+                    if(abs(R(radiFy(iPoint,1))-(xi*cos(Fi(radiFy(iPoint,2)))+yi*sin(Fi(radiFy(iPoint,2)))))<threshold)
+                        lines(xi,yi)=255;
+                    end
+           end     
+           %end
    end
 end
 
 figure(2)
-subplot(2,1,1);
+%subplot(2,1,1);
 imshow(uint8(votingSpace));
 %subplot(2,1,2);
 figure(4)
 imshow(uint8(lines));
+figure(5)
+imshow(Maximus);
+figure(7)
+surf(preMaximus);
