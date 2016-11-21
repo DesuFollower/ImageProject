360ea5e67c1bc5c83b4d8f950d17d792e90fc1ca
alexeifigueroa <alexeifigueroa@gmail.com>
2016-11-21 15:48:31 +0100

    Removed duplicated sampleImage.m Added general parameter Phi to generalized Hough Transform

--- HoughTransform/houghLines/Imagedetect.m
+++ HoughTransform/houghLines/Imagedetect.m
@@ -2,8 +2,9 @@ close all;
 clear;
 clc;
 
-image = sampleImage(200,200);
-image = image.whiteSquare(20);
+%image = sampleImage(25,25);
+%image = image.whiteSquare(20);
+image=rgb2gray(imread('HAW90.png'));
 canniedImage = edge(image, 'canny');
 figure (1)
 imshow(canniedImage);
@@ -27,75 +28,104 @@ for xi=1:height
    end
 end
 
+%%
+dPhi=pi/2;
+Phi=0:dPhi:2*pi-dPhi;
+rotations=zeros(2,2,length(Phi));
+for i=1:length(Phi)
+   rotations(:,:,i)=[cos(Phi(i)) -sin(Phi(i)); sin(Phi(i)) cos(Phi(i))]; 
+end
+
+RotatedByPi=zeros([size(Rtable) length(Phi)]);
+for angle=1:length(Phi)
+    for i=1:size(Rtable,1)
+        RotatedByPi(i,:,angle)=rotations(:,:,angle)*Rtable(i,:)';
+    end
+end
+
+
+%%
 
 %testImage = [zeros(height, width); image];
-testImage = image;
+
+%testImage = image;
+testImage=rgb2gray(imread('HAWRotations.png'));
 cannyTest = edge( testImage, 'canny');
 [height, width]  = size (testImage);
-votingSpace = zeros(3*height, 3*width);
-[pixelcount,bla] = size(Rtable);
+votingSpace = zeros(3*height, 3*width,length(Phi));
+pixelcount = size(Rtable,1);
 
 
 for xi=1:height
    for yi=1:width
         if(cannyTest(xi,yi))
-            for centre = 1:pixelcount
-                xv = Rtable(centre,1)+xi+height;
-                yv = Rtable(centre,2)+yi+width;
-                votingSpace(xv,yv) = votingSpace(xv,yv)+1;
-            
+            for angle=1:length(Phi)
+                for centre = 1:pixelcount
+                    xv = RotatedByPi(centre,1,angle)+xi+height;
+                    yv = RotatedByPi(centre,2,angle)+yi+width;
+                    votingSpace(xv,yv,angle) = votingSpace(xv,yv,angle)+1;
+
+                end
             end
         end 
    end
 end
 
-figure (2)
-imshow(votingSpace);
+%figure (2)
+%surf(votingSpace);
 gaussianFilter = fspecial('gaussian', [13,13], 3); 
 
 Gm = abs(gradient(votingSpace));
 preMaximus = imfilter(Gm, gaussianFilter);
 
-votingThreshold=0.8*max(max(preMaximus));
-[height, width] = size(votingSpace);
+votingThreshold=0.8*max(max(max(preMaximus)));
+[height, width] = size(votingSpace(:,:,1));
 
-for xi=1:height
-    for yi=1:width
-        if (preMaximus(xi,yi)< votingThreshold)
-           preMaximus(xi,yi)=0;
+for angle=1:length(Phi)
+    for xi=1:height
+        for yi=1:width
+            if (preMaximus(xi,yi,angle)< votingThreshold)
+               preMaximus(xi,yi,angle)=0;
+            end
         end
-    end
-end 
-
+    end 
+end
 %reducing filtered coordinates to dots on a voting plane
 Maximus=imregionalmax(preMaximus);
 
-newImage = zeros(height, width);
+newImage = zeros(height, width,length(Phi));
 
-numberOfShapes = sum(sum (Maximus));
-centersDetected = zeros(numberOfShapes,2);
+numberOfShapes = sum(sum(sum(Maximus)));
+centersDetected = zeros(numberOfShapes,2,length(Phi));
 index = 1;
 for xi=1:height
     for yi=1:width
-        if (Maximus(xi,yi))
-           centersDetected(index,:)=[xi yi];
-           index=index+1;
+        for angle=1:length(Phi)
+            if (Maximus(xi,yi,angle))
+               centersDetected(index,:,angle)=[xi yi];
+               index=index+1;
+            end
         end
     end
 end   
 
+    
 for i = 1: numberOfShapes
-    for j = 1 : pixelcount
-                xi = centersDetected(i,1)- Rtable(j,1);
-                yi = centersDetected(i,2)- Rtable(j,2);
-                newImage(xi,yi) = 255; 
+    for angle=1:length(Phi)
+        for j = 1 : pixelcount
+            if(centersDetected(i,1,angle)~=0&&centersDetected(i,2,angle)~=0)
+                xi = centersDetected(i,1,angle)- RotatedByPi(j,1,angle);
+                yi = centersDetected(i,2,angle)- RotatedByPi(j,2,angle);
+                newImage(xi,yi,angle) = 255;
+            end
+        end
     end
 end
-
-
+[height, width]  = size (testImage);
 
 figure (3)
-imshow(newImage);
+newImage=sum(newImage,3);
+imshow(uint8(newImage(height:2*height,width:2*width)));
 
-figure(4)
-imshow(preMaximus);
+%figure(4)
+%imshow(preMaximus);
