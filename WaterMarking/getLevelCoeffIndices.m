96f7c240931fa93c105b1711a08ea5d8aae56573
alexeifigueroa <alexeifigueroa@gmail.com>
2016-12-13 21:00:28 +0100

    -Commented getLevelCoeffIndices -Commented and finished the recovering part of the twoDimensionalWatermarking.m script

--- WaterMarking/getLevelCoeffIndices.m
+++ WaterMarking/getLevelCoeffIndices.m
@@ -1,26 +1,35 @@
 function [startC,endC]= getLevelCoeffIndices(bookKeeping,level,maxLevel,intDirection)
-    %0 H direction
-    %1 V direction
-    %2 D direction
-    %-1 for approximations at level maxLevel
+    %Returns the indices within the array described by the bookKeeping
+    %matrix (i.e. created by wavedec2), that correspond to a direction 
+    %(Vertical, Horizontal or Diagonal ) and a level within the maxLevel 
+    %ranges. For Approximation level should be max level and intDir -1
+    %@bookKeeping:  Second Return value of wavedec2
+    %@level:        level for which the coefficient indices should be
+    %               returned
+    %@maxLevel:     Level of decomposition using wavedec2
+    %@intDirection: Integer denoting the direction of the coefficients i.e.
+    %               0 Horizontal direction
+    %               1 Vertical direction
+    %               2 Diagonal direction
+    %              -1 for approximations at level maxLevel
     
-    row=maxLevel-level+2;
-    currentSize=bookKeeping(row,1)*bookKeeping(row,2);
+    row=maxLevel-level+2;%Row of the corresponding level within the bookkeeping matrix
+    currentSize=bookKeeping(row,1)*bookKeeping(row,2);%rows*columns
     startC=0;
-    multiplier=0;
-    
+     
     for i=2:row-1
-        multiplier=3;
-        if(i==2)
+        multiplier=3;%for all of the levels except the first
+        if(i==2)%for the first level
             multiplier=4;  
         end
+        %Add up all the levels previous to the one of interest
         startC=startC+bookKeeping(i,1)*bookKeeping(i,2)*multiplier;
     end
-    if(level==maxLevel)
-        startC=currentSize;
+    
+    if(level==maxLevel)%for the last level of the decomposition
+        startC=currentSize;%use rows*columns since there are no further coefficients
     end    
-    startC=startC+intDirection*currentSize+1;
-    endC=startC+currentSize-1;
-       
-        
+    startC=startC+intDirection*currentSize+1;%starting index
+    endC=startC+currentSize-1;%ending index
+               
 end
--- WaterMarking/twoDimensionalWatermarking.m
+++ WaterMarking/twoDimensionalWatermarking.m
@@ -6,43 +6,76 @@ hostImage=rgb2gray(imread('Publication3.png'));
 watermarkImage=rgb2gray(imread('WaterMark.png'));
 
 levels=4;
+%Wavelet Decomposition
 [CHost,Sh]=wavedec2(hostImage,levels,'haar');
 [CWater,Sw]=wavedec2(watermarkImage,levels,'haar');
 
-q=0.01;
-p=(1-q);
+q=0.01;%Weight of watermark coefficients
+p=(1-q);%Weight of Host image
 waterMarkedC=zeros(1,length(CHost));
-directions=2;
+directions=2;%0 horizontal, 1 vertical, 2 diagonal
 
 for i=1:levels
-    
+    %for each level
     for j=0:directions
+        %Find the indices corresponding to the coefficients of each
+        %direction
         [cHstart,cHend]=getLevelCoeffIndices(Sh,i,levels,j);
         [cWstart,cWend]=getLevelCoeffIndices(Sw,i,levels,j);
+        %weight the host and watermark coefficients with the parameters
         waterMarkedC(1,cHstart:cHend)=CHost(cHstart:cHend).*p+CWater(cWstart:cWend).*q;
 
     end
     
 end
-
+%Retrieve the aproximation coefficient indices
 [cHstart,cHend]=getLevelCoeffIndices(Sh,levels,levels,-1);
 [cWstart,cWend]=getLevelCoeffIndices(Sw,levels,levels,-1);
+%weight the host and watermark approximation coefficients with the parameters
 waterMarkedC(1,cHstart:cHend)=CHost(cHstart:cHend).*(1-q*q)+CWater(cWstart:cWend).*(q*q);
 
+%Recompose the watermarked Image
 waterMarkedImage=waverec2(waterMarkedC,Sh,'haar');
 
+%recovering the Watermark
+recWaterMarCoefficients=zeros(1,length(CHost));
 
+for i=1:levels
+    %for each level
+    for j=0:directions
+        %Find the indices corresponding to the coefficients of each
+        %direction
+        [cHstart,cHend]=getLevelCoeffIndices(Sh,i,levels,j);%redundant just in case we have
+        [cWstart,cWend]=getLevelCoeffIndices(Sw,i,levels,j);% different sizes... 
+        %Watermark=(Watermarked-hostImage*weightHost)/weightWatermark
+        recWaterMarCoefficients(1,cHstart:cHend)=(waterMarkedC(cWstart:cWend)-CHost(cHstart:cHend).*p)./q;
 
+    end
+    
+end
 
+%Retrieve the aproximation coefficient indices
+[cHstart,cHend]=getLevelCoeffIndices(Sh,levels,levels,-1);%redundant just in case we have
+[cWstart,cWend]=getLevelCoeffIndices(Sw,levels,levels,-1);% different sizes... 
+%Watermark=(Watermarked-hostImage*weightHost)/weightWatermark
+recWaterMarCoefficients(1,cHstart:cHend)=(waterMarkedC(cWstart:cWend)-CHost(cHstart:cHend).*(1-q*q))./(q*q);
 
-
-
-
-
+%Recompose the watermarked Image
+recoveredWatermark=waverec2(recWaterMarCoefficients,Sh,'haar');
 
 figure(1)
-imshow(uint8(waterMarkedImage));
-figure(2)
+subplot(2,2,1);
 imshow(hostImage);
+title('Host image');
+subplot(2,2,2);
+imshow(watermarkImage);
+title('WaterMark image');
+subplot(2,2,3);
+imshow(uint8(waterMarkedImage));
+title('Watermarked image');
+subplot(2,2,4);
+imshow(uint8(recoveredWatermark));
+title('Recovered Watermark ');
+
 
 
