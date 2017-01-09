0ea5ef1e95be7f60a92a5c1cf81c8e30fce5b4a3
Sergei Smekhunov <DesuFollower@users.noreply.github.com>
2017-01-09 14:55:15 +0100

    Update houghLines_surroundings.m
    added comments

--- HoughTransform/houghLines/houghLines_surroundings.m
+++ HoughTransform/houghLines/houghLines_surroundings.m
@@ -19,17 +19,36 @@ R=0:dR:maxR-dR;
 
 votingSpace=zeros(rSteps,FiSteps);
 
-% maximal deviation of point from the expected line
-threshold=1;
-
 cosFi=cos(Fi);
 sinFi=sin(Fi);
+% Here lies the main difference between this method and usual hough lines
+% detection algorithm
+% Instead of checking all possible angles for one point, we use directions
+% Each direction covers the range of angles
+% z.B. if we have distance=1 , we have eight possible directions for point,
+% for distance=2, we have 16 possible directions (here displayed with *)
+% however, to prevent double-scanning of the same line we go only through
+% half of the cycle (here p-point that we are currentyl checking, d -
+% doubles of directions)
+% distance =1 d**       example: 010            distance =2 dd***
+%             dp*                010                        d###*
+%             dd*                000                        d#p#*
+%                                                           d###*
+%                                                           ddd**
+% thus, if p(point)=1 , we start checking all direction points for being=1
+% as well, if they are - we translate the angle (range of angles) 
+% into phi ( angle of normal line to line that we have detected) and calculate r
+% in our example we have a vertical direction detected, which covers angles
+% 68 to 112 (90+-22). or -22 to 22 if we translate it to normal form.
+% now we calculate r for each of those values and put them into table
 
 distance=3;
+%getDirectionMap creates an array with directions referece points
 directionMap =getDirectionMap(distance);
 directionMapLength= distance*4;
 angleSteps= FiSteps/3*2;
 sectorLength = fix(angleSteps/directionMapLength);
+% mapping range of angles to each direction
 angleMap = int16(zeros (directionMapLength,sectorLength));
 for i = 0:angleSteps-1;
     sector = fix(i/sectorLength)+1;
@@ -43,6 +62,9 @@ for xi=distance+1:1:height-distance
    for yi=distance+1:1:width-distance
         if(canniedImage(xi,yi))
                currentSin=yi/(sqrt(xi^2+yi^2));
+            %Determining sin of current angle
+            % Here we split directions into two parts
+            % So it's easier for us to determine resulting normal angle.
             for direction=1:directionMapLength/2
                 C=directionMap(direction,:);
                 if (canniedImage(xi+C(1), yi+C(2)))
@@ -91,7 +113,7 @@ preMaximus =imfilter(Gm,gaussianFilter);
 
 %applying threshold to voting matrix, throwing away values that are less
 %than 30% of maximum
-votingThreshold=0.5*max(max(preMaximus));
+votingThreshold=0.6*max(max(preMaximus));
 for iR=1:rSteps
     for iFi=1:FiSteps
         if (preMaximus(iR,iFi)< votingThreshold)
@@ -115,6 +137,9 @@ for iR=1:rSteps
         end
     end
 end   
+
+% maximal deviation of point from the expected line
+threshold=1;
 
 %drawing an image to show that lines detected match the original image
 imageWithLines=zeros(height,width);
@@ -144,12 +169,16 @@ title('Original image');
 subplot(2,1,2);
 imshow(uint8(imageWithLines));
 title('Detected lines');
+% Showing resulting voting space
 figure(3)
 surf(votingSpace);
 title('Voting space');
+% Showing voting space with highlighted maximums
+
 figure(4)
 surf(preMaximus);
 title('Filtered voting space');
+% Voting space in 2 dimensions and detected maximums
 figure(5)
 subplot(2,1,1);
 imshow(uint8(votingSpace));
